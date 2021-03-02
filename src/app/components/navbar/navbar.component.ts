2b7afab9c2819b21b5efe58f9aefc68791d7ce40
Vishal <vishal.vishalmishra.mishra2@gmail.com>
2021-03-02 23:11:16 +0530

    scroll functionality for nav

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -8,11 +8,23 @@ import {Component, HostListener, Input, OnInit, ViewChild} from '@angular/core';
 export class NavbarComponent implements OnInit {
 
 	isMenuCollapsed = true;
+	displayDark = false;
 
 	constructor() {
 	}
 
 	ngOnInit(): void {
+	}
+
+	@HostListener('window:scroll', [])
+	onWindowScroll() {
+		if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+			document.getElementById('nav-bar').classList.add('fixed-nav');
+			this.displayDark = true;
+		} else {
+			document.getElementById('nav-bar').classList.remove('fixed-nav');
+			this.displayDark = false;
+		}
 	}
 
 	clickedMenuItem(event) {
