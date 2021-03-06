0f00b210a872f7135b2365f313fd3a0b6421aa79
Vishal <vishal.vishalmishra.mishra2@gmail.com>
2021-03-07 00:41:20 +0530

    job component html and css

--- src/app/components/job/job.component.ts
+++ src/app/components/job/job.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-job',
-  templateUrl: './job.component.html',
-  styleUrls: ['./job.component.scss']
+	selector: 'app-job',
+	templateUrl: './job.component.html',
+	styleUrls: ['./job.component.scss']
 })
 export class JobComponent implements OnInit {
 
-  constructor() { }
+	@Input() job: Job;
 
-  ngOnInit(): void {
-  }
+	constructor() {
+	}
+
+	ngOnInit(): void {
+	}
 
 }
+
+export interface Job {
+	title: string;
+	company: string;
+	location: string;
+	type: string;
+	from: string;
+	to: string;
+}
+
--- src/app/components/resume/resume.component.ts
+++ src/app/components/resume/resume.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {Job} from '../job/job.component';
 
 @Component({
 	selector: 'app-resume',
@@ -7,7 +8,27 @@ import {Component, OnInit} from '@angular/core';
 })
 export class ResumeComponent implements OnInit {
 
+	jobList: Job[];
+
 	constructor() {
+		this.jobList = [
+			{
+				company: 'Societe Generale',
+				location: 'Bangalore',
+				from: 'June 2019',
+				to: 'Present',
+				title: 'Software Engineer',
+				type: 'FULLTIME'
+			},
+			{
+				company: 'IDeaS Revenue Solutions',
+				location: 'Pune',
+				from: 'July 2018',
+				to: 'December 2018',
+				title: 'Software Engineer',
+				type: 'INTERN'
+			}
+		]
 	}
 
 	ngOnInit(): void {
